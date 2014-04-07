60d7069fab91d4c92f7b889de959f0c19f23da59
Theo Inglis <theo.inglis@gmail.com>
2014-04-07 17:44:12 +0100

    Auth

---
+++ Filters/ExceptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace Common.Web.Filters
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            base.OnException(context);
+        }
+    }
+}
